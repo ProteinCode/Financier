976b97881f67d2ea329e915a0322275d99cd74b5
ProteinCode <proteincode@163.com>
2022-10-12 00:35:35 +0800

    Add navigation
    Add navigation on the home page

--- financier_front/src/main.js
+++ financier_front/src/main.js
@@ -5,6 +5,7 @@ import installElementPlus from './plugins/element'
 import axios from 'axios'
 import md5 from 'js-md5'
 import cookie from 'js-cookie'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 installElementPlus(app)
@@ -16,3 +17,7 @@ app.config.globalProperties.$md5 = md5
 app.config.globalProperties.$cookie = cookie
 
 axios.defaults.baseURL = '/api'
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
--- financier_front/src/views/Home.vue
+++ financier_front/src/views/Home.vue
@@ -1,12 +1,49 @@
 <template>
-  <div>
-    <h1>Home vue.</h1>
-  </div>
+  <el-affix :offset="0">
+    <nav class="home-nav">
+        <div style="width: 70%; margin: auto auto">
+          <div style="float:left; color: white; font-size: 25px; margin-top: 11px;">
+            <a href="#">Home</a>
+          </div>
+          <el-dropdown :hide-on-click="false" style="margin: 8.5px 0 0 0;float:right">
+            <el-avatar :size="33" src="https://empty" @error="errorHandler">
+              <img
+                src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+              />
+            </el-avatar>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item>
+                  <el-icon><House /></el-icon>
+                  个人中心
+                </el-dropdown-item>
+                  <el-dropdown-item disabled>
+                    <el-icon><Operation /></el-icon>
+                    管理用户
+                  </el-dropdown-item>
+                  <el-dropdown-item divided>
+                    <el-icon><SwitchButton /></el-icon>
+                    退出
+                  </el-dropdown-item>
+                </el-dropdown-menu>
+              </template>
+          </el-dropdown>  
+        </div>
+    </nav>
+  </el-affix>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue'
+
 </script>
 
 <style>
-
+.flex-grow {
+  flex-grow: 1;
+}
+.home-nav {
+  background-color: #2287d6;
+  height: 55px;
+}
 </style>
